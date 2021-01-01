0e43cfa41787c97b7feeb9f0b1932d0f6ae4dd9e
Jan Oberhauser <jan.oberhauser@gmail.com>
2021-01-01 23:16:28 +0100

    :sparkles: Make it possible to make GET & POST requests

--- dash.sh
+++ dash.sh
@@ -28,16 +28,42 @@ then
     exit 0
 fi
 
-url=${commands[$command]}
+method=GET
+data=${commands[$command]}
+
+if [[ $data = *"|"* ]]; then
+    # 
+    IFS='|' read -r -a array <<< "$data"
+
+    for element in "${array[@]}"
+    do
+        if [[ $element = "URL:"* ]]; then
+            url="${element:4}"
+        elif [[ $element = "METHOD:"* ]]; then
+            method="${element:7}"
+        fi
+    done
+
+    if [ -z "$url" ]; then
+        echo "There is no URL defined for command \"$command\"!"
+        exit 0
+    fi
+else
+    url=$data
+fi
 
 if [ "${@: -1}" == "--test" ]
 then
     echo "*** TEST WEBHOOK ***"
     url="${url/\/webhook\//\/webhook-test\/}"
-    parameter="${*:2:$#-2}" 
+    parameter=${*:2:$#-2}
 else
     parameter=${*:2}
 fi
 
-echo -e "$(curl -s -d "command=$command&parameter=$parameter" -H "user-agent: bash-dash" -X POST $url)"
+if [[ $method = "POST" ]]; then
+    echo -e "$(curl -s --data-urlencode "command=$command" --data-urlencode "parameter=$parameter" -H "user-agent: bash-dash" -X POST -G $url)"
+else
+    echo -e "$(curl -s --data-urlencode "command=$command" --data-urlencode "parameter=$parameter" -H "user-agent: bash-dash" -G $url)"
+fi
 echo ""
