5855ec7944f9c6e4f4b6a9d4fbea4f0e28c6b996
Jan Oberhauser <jan.oberhauser@gmail.com>
2021-01-03 13:13:25 +0100

    :sparkles: Make it possible to supply custom test-url

--- dash.sh
+++ dash.sh
@@ -4,7 +4,7 @@ declare -A commands
 
 # Define the commands here
 commands[test]="http://localhost:5678/webhook/test"
-commands[test2]="URL:http://localhost:5678/webhook/test|METHOD:POST"
+commands[test2]="URL:http://localhost:5678/webhook/test|METHOD:POST|TEST-URL:http://localhost:5678/webhook-test/test"
 
 # ----------------------------------
 # ------ NO CHANGES PAST HERE ------
@@ -40,6 +40,8 @@ if [[ $data = *"|"* ]]; then
     do
         if [[ $element = "URL:"* ]]; then
             url="${element:4}"
+        elif [[ $element = "TEST-URL:"* ]]; then
+            testurl="${element:8}"
         elif [[ $element = "METHOD:"* ]]; then
             method="${element:7}"
         fi
@@ -56,8 +58,12 @@ fi
 if [ "${@: -1}" == "--test" ]
 then
     echo "*** TEST WEBHOOK ***"
-    url="${url/\/webhook\//\/webhook-test\/}"
     parameter=${*:2:$#-2}
+    if [ -z "$testurl" ]; then
+        url="${url/\/webhook\//\/webhook-test\/}"
+    else
+        url=$testurl
+    fi
 else
     parameter=${*:2}
 fi
